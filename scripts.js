1ce58077badfd198daa13de1a96b4efebe966d34
Nigel Orion <nigel.orion@Carthooks-MacBook-Pro-2.local>
2019-06-11 11:47:05 -0700

    testing url params

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 function testFire() {
-    console.log("it is working fine...")
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == 'code'){return pair[1];}
+    }
+    return(false)
 }
