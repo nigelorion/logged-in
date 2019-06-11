3a884ceb41b09114a1b655f49eaa001af113ecff
Nigel Orion <nigel.orion@Carthooks-MacBook-Pro-2.local>
2019-06-11 14:21:37 -0700

    testing request object params

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
-function testFire() {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
-            if(pair[0] == 'code'){
-                console.log(pair[1]);
-                requestToken(pair[1]);
-
-            }
-    }
-    return(false)
-}
-
 let request = {
     "grant_type": "authorization_code",
     "client_id": "313363a4fee6f7509ab386fd0dc31c6d",
@@ -20,8 +6,22 @@ let request = {
     "redirect_uri": "https://partners.carthooklocal.com/callback"
 }
 
-function requestToken(code) {
-    request.code = code;
+function testFire() {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == 'code'){
+                console.log(pair[1]);
+                request.code = pair[1];
+                requestToken();
+
+            }
+    }
+    return(false)
+}
+
+function requestToken() {
     return fetch("https://api.partners.dev.carthook.com/oauth/token", {
         method: 'post',
         headers: {
